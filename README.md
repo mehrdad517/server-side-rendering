3196e869f5aed6a1eed5e42cf2001bc898dbe0da
mehrdad517 <mehrdad.masoumi517@gmail.com>
2019-08-15 12:10:20 +0430

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## server side rendering with react redux and next.js
+
+ssr render all html tags in dom
+
+seo and speed nice
+
+##install project
+npm run dev
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Index() {
+  return (
+    <div className="App">
+      <h1>server side rendering</h1>
+      <p>press cntr + u or view page source</p>
+    </div>
+  );
+}
+
+export default Index;
